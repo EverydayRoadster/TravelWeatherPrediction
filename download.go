46288d2d59c252324b34c16a1ac40273d0fc4507
Everyday Roadster <everydayroadster@gmail.com>
2026-02-18 23:39:42 +0100

    history loading

--- download.go
+++ download.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 )
 
 const baseURL = "https://www.cpc.ncep.noaa.gov/products/CFSv2/"
+const historyURL = "https://www.cpc.ncep.noaa.gov/products/CFSv2/cfsv2_fcst_history/"
 
 var variables = map[string]string{
 	"Europe_T2m":  "euT2m",
@@ -24,13 +26,14 @@ var runs = []string{"1", "2", "3"}
 // The actual implementation will be provided later.
 func getImages(inputDir string) (string, error) {
 	now := time.Now().UTC()
-	generationMonth := now.Format("2006-01")
+	// 202602
+	generationMonth := now.Format("200601")
 
 	for folderName, varCode := range variables {
 
 		for lead := 1; lead <= 6; lead++ {
 
-			forecastMonth := now.AddDate(0, lead, 0).Format("2006-01")
+			forecastMonth := now.AddDate(0, lead, 0).Format("200601")
 
 			for _, run := range runs {
 
@@ -42,14 +45,43 @@ func getImages(inputDir string) (string, error) {
 					forecastMonth,
 					fmt.Sprintf("%s_%s.png", generationMonth, run),
 				)
+				_, err := os.Stat(savePath)
+				if errors.Is(err, os.ErrNotExist) {
+					err := download(url, savePath)
+					if err != nil {
+						fmt.Println("Error:", err)
+					}
+				}
 
-				err := download(url, savePath)
-				if err != nil {
-					fmt.Println("Error:", err)
+			}
+		}
+	}
+	for history := 0; history < 6; history++ {
+		historyDate := now.AddDate(0, -history, 0)
+		historyMonth := historyDate.Format("200601")
+		for lead := 1; lead <= 6; lead++ {
+			forecastMonth := historyDate.AddDate(0, lead-1, 0).Format("200601")
+			for _, run := range runs {
+				for folderName, varCode := range variables {
+					url := buildHistoryURL(varCode, run, lead, historyMonth)
+					savePath := filepath.Join(
+						inputDir,
+						folderName,
+						forecastMonth,
+						fmt.Sprintf("%s_%s.png", historyMonth, run),
+					)
+					_, err := os.Stat(savePath)
+					if errors.Is(err, os.ErrNotExist) {
+						err := download(url, savePath)
+						if err != nil {
+							fmt.Println("Error:", err)
+						}
+					}
 				}
 			}
 		}
 	}
+
 	return inputDir, nil
 }
 
@@ -60,6 +92,20 @@ func buildCurrentURL(variable, run string, lead int) string {
 	return fmt.Sprintf(
 		"%simagesInd%s/%sMonInd%d.gif",
 		baseURL,
+		run,
+		variable,
+		lead,
+	)
+}
+
+func buildHistoryURL(variable, run string, lead int, historyMonth string) string {
+	// https://www.cpc.ncep.noaa.gov/products/CFSv2/cfsv2_fcst_history/202602/imagesInd1/euT2mMonInd1.gif
+	//	<baseURL><historyMonth>/imagesInd<run>/<variable>MonInd<lead>.gif
+
+	return fmt.Sprintf(
+		"%s%s/imagesInd%s/%sMonInd%d.gif",
+		historyURL,
+		historyMonth,
 		run,
 		variable,
 		lead,
